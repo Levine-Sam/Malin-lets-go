dabc41491e7fef251fd42aac8bf0e00285efcf03
Sam <sl10015@nyu.edu>
2025-04-17 15:24:36 -0400

    eventcard I think is done

---
+++ app/components/ui/HoverEffect.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function HoverEffect() {
+  return (
+    <div className="w-full h-full bg-black/50 absolute top-0 left-0 z-10 pointer-events-none"></div>
+  );
+}
