ebaadf70c9d4e7c673790c49e6d3e938d1bf2f22
Sam <sl10015@nyu.edu>
2025-04-17 12:41:38 -0400

    04/17

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -25,9 +25,14 @@ export default function EventCard({
       </div>
 
       {/* Event Details */}
-      <div className="flex flex-1 flex-col gap-4 p-6">
+      <div className="self-stretch self-stretch px-4 py-2 inline-flex flex-col justify-between items-start">
         {/* Title */}
-        <h3 className="text-3xl font-semibold leading-tight">{title}</h3>
+        {/* <h3 className="text-xl font-medium leading-loose font-['Poppins'] tracking-tight">
+          {title}
+        </h3> */}
+        <div className="self-stretch justify-start text-neutral-0 text-xl font-medium font-['Poppins'] leading-loose tracking-tight">
+          {title}
+        </div>
 
         {/* Date */}
         <div className="flex items-center gap-4">
@@ -38,7 +43,9 @@ export default function EventCard({
           >
             <path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zM5 8V6h14v2H5zm2 4h10v2H7v-2zm0 4h7v2H7v-2z" />
           </svg>
-          <span className="text-xl">{date}</span>
+          <div className="self-stretch justify-start text-neutral-0 text-base font-normal font-['Poppins'] leading-normal tracking-tight">
+            {date}
+          </div>
         </div>
 
         {/* Location */}
@@ -50,7 +57,9 @@ export default function EventCard({
           >
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 0 1 0-5 2.5 2.5 0 0 1 0 5z" />
           </svg>
-          <span className="text-xl">{location}</span>
+          <div className="self-stretch justify-start text-neutral-0 text-base font-normal font-['Poppins'] leading-normal tracking-tight">
+            {location}
+          </div>
         </div>
 
         {/* Price */}
@@ -62,7 +71,9 @@ export default function EventCard({
           >
             <path d="M21.41 11.58l-9-9C12.05 2.22 11.55 2 11 2H4c-1.1 0-2 .9-2 2v7c0 .55.22 1.05.59 1.42l9 9c.36.36.86.58 1.41.58s1.05-.22 1.41-.59l7-7c.37-.36.59-.86.59-1.41s-.23-1.06-.59-1.42zM5.5 7C4.67 7 4 6.33 4 5.5S4.67 4 5.5 4 7 4.67 7 5.5 6.33 7 5.5 7z" />
           </svg>
-          <span className="text-xl">{price}</span>
+          <div className="self-stretch justify-start text-neutral-0 text-base font-normal font-['Poppins'] leading-normal tracking-tight">
+            {price}
+          </div>
         </div>
 
         {/* Host */}
@@ -74,7 +85,9 @@ export default function EventCard({
           >
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
           </svg>
-          <span className="text-xl">{host}</span>
+          <div className="self-stretch justify-start text-neutral-0 text-base font-normal font-['Poppins'] leading-normal tracking-tight">
+            {host}
+          </div>
         </div>
       </div>
     </div>
