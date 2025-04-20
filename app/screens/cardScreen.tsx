cf2f88e8938d2a01aa711b9f8a23c7c583e7f0d5
Sam <sl10015@nyu.edu>
2025-04-20 16:44:07 -0400

    inline

--- app/screens/cardScreen.tsx
+++ app/screens/cardScreen.tsx
@@ -1,18 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Button } from "@heroui/react";
 
 export default function CardScreen() {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup function to remove the event listener
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty dependency array ensures this runs only on mount and unmount
+  // Empty dependency array ensures this runs only on mount and unmount
 
   return (
     <div className=" h-screen w-screen bg-[#333333] flex justify-center items-center">
@@ -26,21 +16,35 @@ export default function CardScreen() {
             alt="wrapper"
             className="absolute inset-0 w-full h-full object-cover"
           />
-          <div className="absolute bottom-4 right-4 px-4 py-1 bg-black/50 rounded-3xl inline-flex justify-start items-center gap-2">
-            <div className="text-center justify-start text-white text-sm font-normal font-['Poppins'] leading-normal">
+          <div className="absolute bottom-4 right-4 px-4 py-1 bg-black/50 rounded-3xl inline-flex justify-start items-center gap-2 ">
+            <Button className="text-center justify-start text-white text-sm font-normal font-['Poppins'] leading-normal bg-transparent">
               Change Cover Image
+            </Button>
+            {/*<div*/}
+            {/*  data-size="36px"*/}
+            {/*  className="w-9 h-9 flex justify-center items-center"*/}
+            {/*>*/}
+            <div className="w-9 h-9 relative overflow-hidden">
+              <div className="w-7 h-7 left-[3px] top-[3px] absolute bg-neutral-0" />
+              <img src="/icons/iconWrapper.svg" alt="photo" />
             </div>
-            <div
-              data-size="36px"
-              className="w-9 h-9 flex justify-center items-center"
-            >
-              <div
-                data-color="white"
-                data-type="image 1"
-                className="w-9 h-9 relative overflow-hidden"
-              >
-                <div className="w-7 h-7 left-[3px] top-[3px] absolute bg-neutral-0" />
+            {/*</div>*/}
+          </div>
+        </div>
+        <div className="self-stretch inline-flex flex-col items-start justify-start gap-8">
+          <div className="self-stretch flex flex-col justify-start items-start">
+            <div className="flex flex-row gap-2 items-center ">
+              <img
+                className="h-6 w-6"
+                src="/icons/iconWrapper.svg"
+                alt="icon"
+              />
+              <div className="text-neutral-300 text-base font-normal font-['Poppins'] leading-normal tracking-tight">
+                Event Host Name Placeholder
               </div>
+            </div>
+            <div className="self-stretch text-neutral-100 text-5xl font-medium font-['Poppins'] leading-[72px] tracking-wide">
+              Event Name
             </div>
           </div>
         </div>
